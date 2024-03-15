69cf7a6e02203601dbb7dab96a0bec42332b42cf
thegoddess777 <61620529+thegoddess777@users.noreply.github.com>
2024-03-15 12:24:42 +0900

    Add files via upload

---
+++ chp1ex3.cpp
@@ -0,0 +1,17 @@
+// 1-3
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	{
+		const std::string s = "a string";
+		std::cout << s << std::endl;
+	}
+	{
+		const std::string s = "another string";
+		std::cout << s << std::endl;
+	}
+	return 0;
+}
